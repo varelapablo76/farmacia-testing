f3f2ce9f2a248106876f311252f8087f1f233995
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2022-08-05 11:05:17 -0300

    updated for emergency

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -1,36 +1,36 @@
 import Carousel from "react-bootstrap/Carousel";
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player' 
 
 const Carrousel = () => {
   return (
-    <Carousel variant="dark" controls={true} indicators={false} fade className='w-auto h-auto'  >
-      <Carousel.Item className='carousel-img' >
+    <Carousel variant="dark" controls={false} indicators={false} fade className='w-auto h-auto'  >
+      <Carousel.Item className='carousel-img' interval={2000} >
         <img
           className="d-block w-100"
-          src="offer/v1.5BannerTV-13.png"
+          src='offer/BannerTV-49-1.jpg'
           alt="First slide"
         />
       </Carousel.Item>
 
-      <Carousel.Item className='carousel-img' >
+      <Carousel.Item className='carousel-img' interval={2000} >
         <img
-          className="d-block w-100"
-          src="offer/v1.5BannerTV-13.png"
-          alt="First slide"
-        />
-      </Carousel.Item>
-
-      <Carousel.Item className='carousel-img'>
-        <img
-          className="d-block w-100"
-          src="offer/v1.5BannerTV-14.png"
+          className="img-fluid"
+          src="offer/BannerTV-49-2.jpg"
           alt="Second slide"
         />
       </Carousel.Item>
 
-      <Carousel.Item className='carousel-img'>
+      <Carousel.Item className='carousel-img'interval={2000} >
+        <img
+          className="d-block img-fluid"
+          src="offer/BannerTV-49-3.jpg"
+          alt="Tercer slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item className='carousel-img'interval={15000} >
           <ReactPlayer
-          url='offer/vichy-1.mp4'
+          url="offer/eucerin1.mp4"
           width='100%'
           height='100%'
           controls={false}
@@ -38,10 +38,6 @@ const Carrousel = () => {
           muted={true}
           loop={true}
           />
-      {/* <video style={{height:'100%',width:'100%'}} autoplay>
-        <source src="offer/vichy-1.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-        </video> */}
 
       </Carousel.Item>
     </Carousel>
